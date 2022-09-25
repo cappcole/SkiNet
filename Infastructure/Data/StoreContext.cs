030c5ac5b3cd43e417fcdb2fb54f4c26645ddb3b
cappcole <cole.delavergne@gmail.com>
2022-09-25 08:48:53 -0500

    Commit Initial

---
+++ Infastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Core.Entities;
+
+namespace Infastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
